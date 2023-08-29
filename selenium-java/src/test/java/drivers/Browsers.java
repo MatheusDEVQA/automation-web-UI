e1af3ba3b510393220545fd87f3d536ce0a65ad8
MatheusDEVQA <matheus_muniz3@hotmail.com.br>
2023-08-28 21:28:01 -0300

    Add metodos

--- selenium-java/src/test/java/drivers/Browsers.java
+++ selenium-java/src/test/java/drivers/Browsers.java
@@ -1,18 +1,17 @@
 package drivers;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Browsers {
 
-    WebDriver drive;
+    public static WebDriver drive;
 
-    @Test
-    public void abriBrowser(){
-        WebDriverManager.chromedriver().setup();
-        drive = new ChromeDriver();
+
+    public void abrirBrowser() {
+        WebDriverManager.firefoxdriver().setup();
+        drive = new FirefoxDriver();
         drive.manage().window().maximize();
         drive.get("https://ultimateqa.com/automation/");
 
---
+++ selenium-java/src/test/java/metodos/MetodosTeste.java
@@ -0,0 +1,28 @@
+package metodos;
+
+import drivers.Browsers;
+import org.openqa.selenium.By;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MetodosTeste extends Browsers {
+
+    public void escrever(By elemento, String texto){
+        drive.findElement(elemento).sendKeys();
+    }
+
+    public void clicar(By elemento){
+        drive.findElement(elemento).click();
+    }
+
+    public void validarTexto(By elemento, String texto){
+        assertEquals(texto,drive.findElement(elemento).getText());
+    }
+     public void validaContemTextoAlert(String texto){
+        String textAlert = drive.switchTo().alert().getText();
+        assertTrue(textAlert.contains(texto));
+     }
+
+
+}
